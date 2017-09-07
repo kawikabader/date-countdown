0fddfdc42539b2659e8f6bb1962b503f98625264
Kawika Bader <ekbader@gmail.com>
2017-09-07 07:44:36 -0700

    Created app component. Imported app component to render to root element.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
 
-ReactDom.render(
-  <div>date countdown</div> , document.getElementById('root')
-);
+ReactDom.render(<App />, document.getElementById("root"));
