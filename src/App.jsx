a173c085b52de5d30ce8ee8ed8b5a2d5aa77549a
Kawika Bader <ekbader@gmail.com>
2017-09-08 06:51:05 -0700

    Added new deadline state property. Updated changeDeadline method to set new deadline for deadline. Added on change handler to input to update new deadline state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      deadline: 'December 24, 2017'
-    }
+      deadline: "December 24, 2017",
+      newDeadline: ""
+    };
   }
 
   changeDeadline() {
     this.setState({
-      deadline: 'November 25, 2017'
-    })
+      deadline: this.state.newDeadline
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <div className="App-title">
-          Countdown to {this.state.deadline}
-          </div>
+        <div className="App-title">Countdown to {this.state.deadline}</div>
         <div>
           <div className="Clock-days">14 days</div>
           <div className="Clock-hours">4 hours</div>
@@ -29,7 +28,11 @@ class App extends Component {
           <div className="Clock-seconds">20 seconds</div>
         </div>
         <div>
-          <input placeholder="new date" />
+          <input
+            placeholder={this.state.deadline}
+            onChange={event =>
+              this.setState({ newDeadline: event.target.value })}
+          />
           <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
